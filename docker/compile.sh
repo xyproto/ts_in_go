5ebad0c0b57afceadf6ceada88c1b162116104f2
Alexander F Rødseth <rodseth@gmail.com>
2017-10-16 14:46:53 +0200

    Minor improvement

--- docker/compile.sh
+++ docker/compile.sh
@@ -3,8 +3,7 @@ mkdir -p ./bin
 docker run -v "$(pwd)/ts:/ts" -v "$(pwd)/bin:/bin" -t --rm ts_to_go:latest
 if [ -f bin/main ]; then
   echo 'Compilation succeeded.'
-  mv -f bin/main ./main
-  rmdir ./bin
+  mv -f bin/main ./main && rmdir ./bin
   ls -al --color=auto -F ./main
 else
   echo 'Compilation failed.'
